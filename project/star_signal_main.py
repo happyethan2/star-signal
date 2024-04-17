f1c0192f59561bb0b31149b0b646ce2703cee0a9
Ethan Colgan <ethancolgan@adam.com.au>
2024-04-17 19:33:18 +0930

    added swagger_client package

--- project/star_signal_main.py
+++ project/star_signal_main.py
@@ -1,3 +1,4 @@
 import star_signal_utils as utils
+import config
 
 test = utils.get_clouds()
--- project/star_signal_utils.py
+++ project/star_signal_utils.py
@@ -1,2 +1,24 @@
-def get_clouds():
+import requests
+import json
+import config
+
+### RapidAPI - Utility Functions
+def get_moon_phase(lat, lon):
+    url = "https://moon-phase.p.rapidapi.com/advanced"
+
+    querystring = {"lat": lat,"lon": lon}
+
+    headers = {
+        "X-RapidAPI-Key": "6c925f5fc2msh9b0d7fdec48e8a5p1e3e99jsndb79e168c058",
+        "X-RapidAPI-Host": "moon-phase.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    response = json.dumps(response.json(), indent=4)
+
+    return response
+
+
+### Weather API - Utility Functions
+def get_clouds(lat, lon):
     return None
