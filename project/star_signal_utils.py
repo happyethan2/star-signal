3687c7cb73a717ccf83a72a4206abbc8ebefa8f7
Ethan Colgan <ethancolgan@adam.com.au>
2024-04-18 18:07:01 +0930

    changed git commit file to exclude ds store files that are exclusive to macos

--- project/star_signal_utils.py
+++ project/star_signal_utils.py
@@ -1,7 +1,12 @@
 import requests
 import json
-import config
 import swagger_client
+
+from __future__ import print_function
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
+from config import WEATHER_API_KEY
 
 ### RapidAPI - Utility Functions
 def get_moon_phase(lat, lon):
@@ -22,4 +27,20 @@ def get_moon_phase(lat, lon):
 
 ### Weather API - Utility Functions
 def get_clouds(lat, lon):
-    return None
+    # Configure API key authorization: ApiKeyAuth
+    configuration = swagger_client.Configuration()
+    configuration.api_key['key'] = WEATHER_API_KEY
+    # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+    # configuration.api_key_prefix['key'] = 'Bearer'
+
+    # create an instance of the API class
+    api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
+    q = '-34.6316667,139.6675000' # str | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more.
+    dt = '2024-04-16' # date | Date on or after 1st Jan, 2015 in yyyy-MM-dd format
+
+    try:
+        # Astronomy API
+        api_response = api_instance.astronomy(q, dt)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling APIsApi->astronomy: %s\n" % e)
