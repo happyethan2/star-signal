5c571b0f3f41ffebf4bc7991f10496f3983cb5d7
Ethan Colgan <ethancolgan@adam.com.au>
2024-06-09 19:56:26 +0930

    new scheduling changes

--- project/star_signal_main.py
+++ project/star_signal_main.py
@@ -43,36 +43,57 @@ def check_forecast():
     
     notifs.send_push_notification(config.USERS['Ethan'], notification, 'Ethan') if total_score >= 10.0 else None
 
-import time
-from datetime import datetime, timedelta
 
 def task():
-    try:
-        logging.info("Running task...")
-        check_forecast()
-        logging.info("Task completed.")
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
+    logging.info("Running task...")
+    check_forecast()
+    logging.info("Task completed.")
+
+from datetime import datetime, timedelta
+import time
+import logging
+
+TASK_HOUR = 10  # [24hr]
+TASK_MINUTE = 0  # [min]
+CHECK_INTERVAL = 600  # [sec]
 
 def main():
     task_done_today = False
+    last_reset_day = None
 
     while True:
         current_time = datetime.now()
-        run_time = current_time.replace(hour=10, minute=0, second=0, microsecond=0)
+        run_time = current_time.replace(hour=TASK_HOUR, minute=TASK_MINUTE, second=0, microsecond=0)
 
-        if current_time >= run_time:
-            if not task_done_today:
-                task()
-                task_done_today = True
-        
-        # reset flag after 9:30am following day
-        if current_time >= run_time - timedelta(minutes=30) + timedelta(days=1) and task_done_today:
+        print(f"Current time: {current_time}, Run time: {run_time}, Task done today: {task_done_today}")
+
+        # if day crosses over to next
+        if last_reset_day is not None and current_time.date() != last_reset_day:
+            print("Day has changed, resetting task_done_today to False")
             task_done_today = False
+            last_reset_day = current_time.date()
 
-        # sleep to avoid cpu use
-        time.sleep(300)  # 5min
+        # if current time past run time and task not done
+        if current_time >= run_time and not task_done_today:
+            try:
+                print("Attempting to run task...")
+                task()
+                print("Task successfully run.")
+                task_done_today = True
+            except Exception as e:
+                logging.error(f"An error occurred: {e}")
+                print("An error occurred while running the task.")
+                pass
 
+        # calculate sleep duration until next task or reset time
+        if task_done_today:
+            next_time = run_time + timedelta(days=1)
+            sleep_duration = (next_time - current_time).total_seconds() - 60 # ensures earliest execution
+        else:
+            sleep_duration = CHECK_INTERVAL
+
+        print(f"Sleeping for {sleep_duration/60/60:.2f} hours...")
+        time.sleep(max(0, sleep_duration))  # sleep until next time, but never sleep a negative amount
 
 if __name__ == "__main__":
     main()
